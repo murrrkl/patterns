0ebf66ae789d83e49b21f3aff404f5a003d84dca
murrrkl <45383340+murrrkl@users.noreply.github.com>
2022-12-04 21:37:26 +1000

    Update observer.cpp

--- observer.cpp
+++ observer.cpp
@@ -1,132 +1,58 @@
-#include <iostream>
-#include <list>
-#include <string>
+//
+// Created by Каринэ Матевосян on 04.12.2022.
+//
+
+#include "observer.h"
 
 using namespace std;
 
-class IClient {
-public:
-    virtual ~IClient(){};
-    virtual void Update(const std::string &message_from_creator) = 0;
-};
+Creator::Creator(string _order)  {
+    this->order = _order;
+    cout << "Hi, I'm the Creator of " << _order <<  endl;
+}
 
-class ICreator {
-public:
-    virtual ~ICreator(){};
-    virtual void Attach(IClient *client) = 0;
-    virtual void Detach(IClient *client) = 0;
-    virtual void Notify() = 0;
-};
+void Creator::Attach(IClient *client) {
+    list_client_.push_back(client);
+}
 
+void Creator::Detach(IClient *client)  {
+    list_client_.remove(client);
+}
 
-class Creator : public ICreator {
-public:
+void Creator::Notify()  {
+    std::list<IClient *>::iterator iterator = list_client_.begin();
+    // HowManyClient();
+    while (iterator != list_client_.end()) {
+        (*iterator)->Update(message_);
+        ++iterator;
+    }
+}
 
-    Creator(string _order) {
-            this->order = _order;
-            cout << "Hi, I'm the Creator your order: ' " << _order << " ' " << endl;
-    }
+void Creator::CreateMessage()  {
+    this->message_ = "Your order: ' " + this->order + " ' is ready";
+    Notify();
+}
 
-    /*
-    virtual ~Creator() {
-        cout << "Goodbye, I was the Creator." << endl;
-    }
-     */
+void Creator::HowManyClient()  {
+    cout << "There are " << list_client_.size() << " clients in the list." << endl;;
+}
 
-/**
-* Методы управления заказами.
-*/
-    void Attach(IClient *client) override {
-        list_client_.push_back(client);
-    }
-    void Detach(IClient *client) override {
-        list_client_.remove(client);
-    }
-    void Notify() override {
-        std::list<IClient *>::iterator iterator = list_client_.begin();
-        // HowManyClient();
-        while (iterator != list_client_.end()) {
-            (*iterator)->Update(message_);
-            ++iterator;
-        }
-    }
+Client::~Client() noexcept  {
+    cout << "Client " << this->number_ << " has received his order" << endl;
+}
 
-    void CreateMessage() {
-        this->message_ = "Your order: ' " + this->order + " ' is ready";
-        Notify();
-    }
-    void HowManyClient() {
-        cout << "There are " << list_client_.size() << " clients in the list." << endl;;
-    }
+void Client::Update(const std::string &message_from_creator)  {
+    message_from_creator_ = message_from_creator;
+    PrintInfo();
+}
 
+void Client::RemoveMeFromTheList()  {
+    creator_.Detach(this);
+    cout << "Client " << number_ << " removed from the list." << endl;
+}
 
-    void SomeBusinessLogic() {
-        this->message_ = "change message message";
-        Notify();
-        cout << "I'm about to do some thing important " << endl;
-    }
-
-private:
-    std::list<IClient *> list_client_;
-    string message_;
-    string order;
-};
-
-class Client : public IClient {
-public:
-    Client(Creator &creator) : creator_(creator) {
-        this->creator_.Attach(this);
-        cout << "Hi, I'm the Client " << ++Client::static_number_ << " and i am waiting for my order" << endl;
-        this->number_ = Client::static_number_;
-    }
-    virtual ~Client() {
-        cout << "Client " << this->number_ << " has received his order" << endl;
-    }
-
-    void Update(const std::string &message_from_creator) override {
-        message_from_creator_ = message_from_creator;
-        PrintInfo();
-    }
-    void RemoveMeFromTheList() {
-        creator_.Detach(this);
-        cout << "Client " << number_ << " removed from the list." << endl;
-    }
-    void PrintInfo() {
-        cout << "Client " << this->number_ << ": a new message is available  " << this->message_from_creator_ << endl;
-    }
-
-private:
-    string message_from_creator_;
-    Creator &creator_;
-    static int static_number_;
-    int number_;
-};
+void Client::PrintInfo()  {
+    cout << "Client " << this->number_ << ": a new message is available  " << this->message_from_creator_ << endl;
+}
 
 int Client::static_number_ = 0;
-
-void ClientCode() {
-
-    Creator *order1 = new Creator("Cheesburger, olivier salad, coffee");
-    Client *client1 = new Client(*order1);
-
-    cout << "*******************************************" << endl;
-
-    Creator *order2 = new Creator("Vegan burger, Vegetables salad, ice-tea");
-    Client *client2 = new Client(*order2);
-
-    cout << "--------------------------------" << endl;
-
-    order1->CreateMessage();
-    delete client1;
-    delete order1;
-
-    cout << "--------------------------------" << endl;
-    order2->CreateMessage();
-    delete client2;
-    delete order2;
-}
-
-int main() {
-    ClientCode();
-    return 0;
-}
